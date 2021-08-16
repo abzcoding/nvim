55a84b6812b577c29939fbbec07cdba15291b247
Abouzar Parvan <abz@sotoon.ir>
2021-08-16 22:16:11 +0430

    even more formatter/linters

--- lua/config/lsp/null-ls.lua
+++ lua/config/lsp/null-ls.lua
@@ -9,16 +9,17 @@ function M.setup()
     sources = {
       nls.builtins.formatting.prettierd,
       nls.builtins.formatting.stylua,
-      nls.builtins.formatting.eslint_d,
       nls.builtins.formatting.black.with { extra_args = { "--fast" } },
       nls.builtins.formatting.isort.with { extra_args = { "--profile", "black" } },
       nls.builtins.formatting.goimports,
       nls.builtins.formatting.shfmt.with { extra_args = { "-i", "2", "-ci" } },
       nls.builtins.diagnostics.hadolint,
+      nls.builtins.diagnostics.eslint_d,
       nls.builtins.diagnostics.shellcheck,
       nls.builtins.diagnostics.markdownlint,
       nls.builtins.diagnostics.selene,
       nls.builtins.diagnostics.vint,
+      nls.builtins.diagnostics.chktex,
       nls.builtins.code_actions.gitsigns,
     },
   }
