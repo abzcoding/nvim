143fe3be6307f1d3fdbedd3090522394d647407f
Abouzar Parvan <abz@sotoon.ir>
2021-08-25 21:53:31 +0430

    better completion from nvim-cmp

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -1,6 +1,19 @@
 local M = {}
+local function check_backspace()
+  local col = vim.fn.col "." - 1
+  if col == 0 or vim.fn.getline("."):sub(col, col):match "%s" then
+    return true
+  else
+    return false
+  end
+end
+
+local function T(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
 M.config = function()
-  -- vim.o.completeopt = "menuone,noselect"
+  vim.o.completeopt = "menuone,noselect"
 
   -- nvim-cmp setup
   local cmp = require "cmp"
@@ -10,6 +23,19 @@ M.config = function()
       format = function(entry, vim_item)
         local icons = require("config.lsp.kind").icons
         vim_item.kind = icons[vim_item.kind]
+        vim_item.menu = ({
+          nvim_lsp = "[L]",
+          emoji = "[E]",
+          path = "[F]",
+          calc = "[C]",
+          vsnip = "[S]",
+          buffer = "[B]",
+        })[entry.source.name]
+        vim_item.dup = ({
+          buffer = 1,
+          path = 1,
+          nvim_lsp = 0,
+        })[entry.source.name] or 0
         return vim_item
       end,
     },
@@ -28,34 +54,54 @@ M.config = function()
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
+      ["<ESC>"] = cmp.mapping.close(),
       ["<CR>"] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Replace,
         select = true,
       },
-      ["<Tab>"] = function(fallback)
+      ["<Tab>"] = cmp.mapping(function(fallback)
         if vim.fn.pumvisible() == 1 then
-          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+          vim.fn.feedkeys(T "<C-n>", "n")
         elseif luasnip.expand_or_jumpable() then
-          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          vim.fn.feedkeys(T "<Plug>luasnip-expand-or-jump", "")
+        elseif check_backspace() then
+          vim.fn.feedkeys(T "<Tab>", "n")
         else
-          fallback()
+          vim.fn.feedkeys(T "<C-Space>") -- Manual trigger
         end
-      end,
-      ["<S-Tab>"] = function(fallback)
+      end, {
+        "i",
+        "s",
+      }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
         if vim.fn.pumvisible() == 1 then
-          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+          vim.fn.feedkeys(T "<C-p>", "n")
         elseif luasnip.jumpable(-1) then
-          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          vim.fn.feedkeys(T "<Plug>luasnip-jump-prev", "")
         else
           fallback()
         end
-      end,
+      end, {
+        "i",
+        "s",
+      }),
     },
     sources = {
       { name = "nvim_lsp" },
       { name = "luasnip" },
       { name = "path" },
-      { name = "buffer" },
+      {
+        name = "buffer",
+        opts = {
+          get_bufnrs = function()
+            local bufs = {}
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+              bufs[vim.api.nvim_win_get_buf(win)] = true
+            end
+            return vim.tbl_keys(bufs)
+          end,
+        },
+      },
     },
   }
 end
